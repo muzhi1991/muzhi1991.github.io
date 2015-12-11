c1b9f09ca422b0e7c5d5b118c2d843f9df8fa0fc
baidu <baidu@MacBook-Pro-2.local>
2015-12-11 16:31:10 +0800

    some seo,and add script init.sh/deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#hexo clean
+hexo generate
+
+# seo优化
+cp -Rf ./bac/public/* ./public/
+
+hexo deploy
+
---
+++ init.sh
@@ -0,0 +1,3 @@
+npm install
+git clone https://github.com/iissnan/hexo-theme-next themes/next
+cp -Rf ./bac/themes/next/* ./themes/next/
