4a06431fde0088628aa2440e4c0b70c1431e23d3
limuzhi <limuzhi@baidu.com>
2017-09-03 18:24:50 +0800

    fixed untracked files

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ npm install hexo-generator-cname --save
 npm install hexo-generator-sitemap --save
 npm install hexo-generator-baidu-sitemap --save
 git clone https://github.com/iissnan/hexo-theme-next themes/next
-cp -Rf ./bac/themes/next/* ./themes/next/
+cp -Rf ./bac/bac_20170903/* ./themes/next/
 
